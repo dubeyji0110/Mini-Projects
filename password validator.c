cf45eddaa451d18a56f75329ffc9237fadf0b1a9
Harshit B <harshitbhatt972@gmail.com>
2020-09-11 21:55:14 +0530

    last update remaing to repeat full code again

--- password validator.c
+++ password validator.c
@@ -1,23 +1,72 @@
 #include<stdio.h>
 #include<string.h>
+int spchar(char pass[])
+{
+	int i,count_spchars=0;
+	for(i=0;i<strlen(pass);i++)
+	{
+		if(pass[i]>=33 && pass[i]<=47)
+		count_spchars++;
+	}
+	return count_spchars;
+}
+int nums(char pass[])
+{
+	int i,count_nums=0;
+	for(i=0;i<strlen(pass);i++)
+	{
+		if(pass[i]<=48 && pass[i]<=57)
+		count_nums++;	
+	}
+	return count_nums;
+}
+int count_uc(char pass[])
+{
+	int i,count=0;
+	for(i=0;i<strlen(pass);i++)
+	{
+		if(pass[i]>=65 && pass[i]<=90)
+		count++;
+	}
+	return count;
+}
+int count_lc(char pass[])
+{
+	int i, count=0;
+	for(i=0;i<strlen(pass);i++)
+	{
+		if(pass[i]>=97 && pass[i]<=122)
+		count++;
+	}
+	return count;
+ } 
 int main()
 {
-	int i,c=0,d=0,e=0;
-	char pass[50];
+	printf("******Password Validator********\n");
+	label:;
+	char pass[50],c;			
 	printf("Enter your password:");
-	scanf("%s",pass);
-	for(i=0;pass[i]!='\0';i++)
+	scanf("%s",pass);							//input pass
+	int uc=count_uc(pass);						//count upper case
+	int lc=count_lc(pass);						//count lower case
+	int count_spchar=spchar(pass);				//count special chars
+	int count_nums=nums(pass);					//count nums
+	if((count_spchar>=2)&&(count_nums>=2)&&(strlen(pass)>=8)&&(lc>1)&&(uc>1))
 	{
-		e=e+1;
-		
-		if((pass[i]=='!')||(pass[i]=='@')||(pass[i]=='#')||(pass[i]=='$')||(pass[i]=='%')||(pass[i]=='&')||(pass[i]=='*'))
-		c++;
-	
-		if((pass[i]=='1')||(pass[i]=='2')||(pass[i]=='3')||(pass[i]=='4')||(pass[i]=='5')||(pass[i]=='6')||(pass[i]=='7')||(pass[i]=='8')||(pass[i]=='9')||(pass[i]=='0'))
-		d++;
+		printf("\nSpecial characters: %d\n\nNumbers: %d\n\nUpper cases: %d\n\nLower cases: %d\n\nTotal Size: %d\n",count_spchar,count_nums,uc,lc,strlen(pass));
+		printf("\n=======Strong=======\n");
 	}
-	if((d>=2)&&(c>=2)&&(e>=8))
-	printf("Strong");
 	else
-	printf("Weak");
+	{
+		printf("\nSpecial characters: %d\n\nNumbers: %d\n\nUpper cases: %d\n\nLower cases: %d\n\nTotal Size: %d\n",count_spchar,count_nums,uc,lc,strlen(pass));
+		printf("\n=======Weak=======\n");
+	}
+	printf("Try Again?[Y/N]");
+	fflush(stdin);
+	scanf("%c",&c);
+	if(c==89 && c==121)
+	{
+		system("cls");
+		goto label;
+	}
 }
