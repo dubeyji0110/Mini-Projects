bd034c88c6e480e58f699fa8b006607529deff6f
Harshit B <harshitbhatt972@gmail.com>
2020-09-11 21:03:30 +0530

    updated

--- password validator.c
+++ password validator.c
@@ -3,17 +3,17 @@
 int main()
 {
 	int i,c=0,d=0,e=0;
-	char n[50];
+	char pass[50];
 	printf("Enter your password:");
-	scanf("%s",n);
-	for(i=0;n[i]!='\0';i++)
+	scanf("%s",pass);
+	for(i=0;pass[i]!='\0';i++)
 	{
 		e=e+1;
 		
-		if((n[i]=='!')||(n[i]=='@')||(n[i]=='#')||(n[i]=='$')||(n[i]=='%')||(n[i]=='&')||(n[i]=='*'))
+		if((pass[i]=='!')||(pass[i]=='@')||(pass[i]=='#')||(pass[i]=='$')||(pass[i]=='%')||(pass[i]=='&')||(pass[i]=='*'))
 		c++;
 	
-		if((n[i]=='1')||(n[i]=='2')||(n[i]=='3')||(n[i]=='4')||(n[i]=='5')||(n[i]=='6')||(n[i]=='7')||(n[i]=='8')||(n[i]=='9')||(n[i]=='0'))
+		if((pass[i]=='1')||(pass[i]=='2')||(pass[i]=='3')||(pass[i]=='4')||(pass[i]=='5')||(pass[i]=='6')||(pass[i]=='7')||(pass[i]=='8')||(pass[i]=='9')||(pass[i]=='0'))
 		d++;
 	}
 	if((d>=2)&&(c>=2)&&(e>=8))
