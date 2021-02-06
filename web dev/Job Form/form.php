55a6c7592b9b1bcec5407b5adfce8cc940cddd96
Harshit Bhatt <harshitbhatt972@gmail.com>
2021-02-07 01:09:49 +0530

    php on job form started

---
+++ web dev/Job Form/form.php
@@ -0,0 +1,16 @@
+<?php
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $gender = $_POST['gender'];
+    $post = $_POST['post'];
+    $yr_of_exp = $_POST['yr_of_exp'];
+    $qualification = $_POST['qualification'];
+    $describe = $_POST['describe'];
+    $project = $_POST['project'];
+    $github = $_POST['github'];
+    $linkedin = $_POST['linkedin'];
+    $email = $_POST['email'];
+    $phn = $_POST['phn'];
+
+    //Database connection
+?>
